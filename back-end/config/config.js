1004bfda098def20e7f39c8a15773dcc93bf3e4d
JLRG806 <jose.rodriguez2@gmail.com>
2024-12-12 07:27:13 +0100

    feat: Update database configuration to use nested options for PostgreSQL and change server port environment variable

--- back-end/config/config.js
+++ back-end/config/config.js
@@ -2,17 +2,22 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-    PORT: process.env.PORT || 5000,
+    PORT: process.env.SERVER_PORT || 5000,
     db: {
-        user: process.env.PG_USER,
-        host: process.env.PG_HOST,
-        database: process.env.PG_DATABASE,
-        password: process.env.PG_PASSWORD,
-        port: process.env.PG_PORT
+        postgres: {
+            options: {
+                user: process.env.PG_USER,
+                host: process.env.PG_HOST,
+                database: process.env.PG_DB,
+                password: process.env.PG_PASSW,
+                port: process.env.PG_PORT,
+                dialect: 'postgres',
+            },
+            client: null
+        }
 
     },
     jwt: {
         secret: process.env.JWT_SECRET
     },
-
 }
