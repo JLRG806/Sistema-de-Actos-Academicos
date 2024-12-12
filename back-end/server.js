5d037bcd809f16023a72cb911c1af4b9823b3d57
JLRG806 <jose.rodriguez2@gmail.com>
2024-12-12 07:27:03 +0100

    feat: Refactor server.js to include database connection and configuration, and update server start logic

--- back-end/server.js
+++ back-end/server.js
@@ -1,21 +1,34 @@
-import express from 'express';
-import morgan from 'morgan';
+import express from 'express'
+import morgan from 'morgan'
+import config from './config/config.js'
+import { connect } from './db/db.js'
+import './models/sequelize/index.js'
 
-const app = express();
 
-app.use(express.json({ limit: "10MB" }));
-//app.options('*', middlewares.cors);
-//app.use(middlewares.cors);
-app.use(morgan('dev'));
+
+const app = express()
+
+
+
+app.use(express.json({ limit: "10MB" }))
+//app.options('*', middlewares.cors)
+//app.use(middlewares.cors)
+app.use(morgan('dev'))
 
 // Routes
 app.get('/api', (req, res) => {
-    res.send([{ msg: 'Hello World Damm son!!'}]);
+    res.send([{ msg: 'Hello World Damm son!!' }])
 })
 
+async function main() {
+    const result = await connect()
+    if (result) {
+        await result.sync()
+        app.listen(config.PORT, () => {
+            console.log(`Server is running on http://127.0.0.1:3000`)
+        })
+    }
+}
 
+main()
 
-// Start the server
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-})
