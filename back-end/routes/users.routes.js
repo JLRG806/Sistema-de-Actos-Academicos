654b1f8396f7ec6e26168d430f2ffec7084aa7f5
JLRG806 <jose.rodriguez2@gmail.com>
2024-12-17 01:46:09 +0100

    feat: Protect user routes with cookieJWTauthAdmin middleware for enhanced security

--- back-end/routes/users.routes.js
+++ back-end/routes/users.routes.js
@@ -6,13 +6,13 @@ const router = Router();
 router.get("/login", usersController.login)
 router.post("/register", usersController.createUser)
 
-router.get("/users", usersController.getUsers)
-router.get("/users/:id", usersController.getUser)
+router.get("/users", cookieJWTauthAdmin, usersController.getUsers)
+router.get("/users/:id", cookieJWTauthAdmin, usersController.getUser)
 
-router.post("/users", usersController.createUser)
+router.post("/users", cookieJWTauthAdmin, usersController.createUser)
 
-router.put("/users/:id", usersController.updateUser)
+router.put("/users/:id", cookieJWTauthAdmin, usersController.updateUser)
 
-router.delete("/users/:id", usersController.deleteUser)
+router.delete("/users/:id", cookieJWTauthAdmin, usersController.deleteUser)
 
 export default router;
