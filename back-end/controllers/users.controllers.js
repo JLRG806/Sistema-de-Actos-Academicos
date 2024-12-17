6f2ee1ab5a4cc1cd8e11b37488ebdccdbca8fccd
JLRG806 <jose.rodriguez2@gmail.com>
2024-12-17 01:11:12 +0100

    feat: Enhance user authentication and management with argon2 hashing and standardized responses

--- back-end/controllers/users.controllers.js
+++ back-end/controllers/users.controllers.js
@@ -1,12 +1,75 @@
-import {User} from '../models/sequelize/User.js'
+import { User } from '../models/sequelize/User.js'
+import argon2 from 'argon2'
+import { defaultResponse } from '../utils/index.js'
+
 export const usersController = {}
+
+usersController.login = async (req, res) => {
+
+    try {
+        const { email, password } = req.body
+
+        if (isAdministrator) {
+
+            defaultResponse.message = 'Logged in as administrator'
+            
+            return res.json(defaultResponse({ message: 'Logged in as administrator' }))
+        }
+
+        const user = await User.findOne({
+            where: {
+                email: email
+            }
+        })
+
+        if (!user) {
+            defaultResponse.error.message = 'User not found'
+            defaultResponse.error.status = true
+            return res.status(404).json(defaultResponse)
+        }
+
+        if (await argon2.verify(user.password, password)) {
+            return res.senStatus(200)
+        } else {
+            return res.status(401).json(defaultResponse({ errorMessage: 'Invalid password', errorStatus: true }))
+        }
+    } catch (error) {
+        return res.status(500).json(defaultResponse({ errorMessage: 'Error logging in', errorStatus: true }))
+    }
+
+}
+
+usersController.register = async (req, res) => {
+    try {
+        const { email, fullName, password } = req.body
+
+        const hashedPassword = await argon2.hash(password)
+
+        const newUser = await User.create({
+            email,
+            fullName,
+            password: hashedPassword,
+            roleId: 2
+        })
+
+        return res.senStatus(201)
+    } catch (error) {
+        console.log(error)
+        if (error.name === 'SequelizeUniqueConstraintError') {
+            return res.status(400).json(defaultResponse({ errorMessage: 'Email already exists', errorStatus: true }))
+        }
+
+
+        return res.status(500).json(defaultResponse({ errorMessage: 'Error creating user', errorStatus: true }))
+    }
+}
 
 usersController.getUsers = async (req, res) => {
     try {
         const users = await User.findAll()
-        res.json(users)
-    }catch (error) {
-        res.status(500).json({ message: "Error getting users" })
+        res.json(defaultResponse({ data: { users: users }, message: 'Users found' }))
+    } catch (error) {
+        res.status(500).json(defaultResponse({ errorMessage: 'Error getting users', errorStatus: true }))
     }
 }
 
@@ -16,12 +79,12 @@ usersController.getUser = async (req, res) => {
         const user = await User.findByPk(id)
 
         if (!user) {
-            return res.status(404).json({ message: "User not found" })
+            return res.status(404).json(defaultResponse({ errorMessage: 'User not found', errorStatus: true }))
         }
 
-        res.json(user)
-    }catch (error) {
-        res.status(500).json({ message: "Error getting user" })
+        res.json(defaultResponse({ data: { user: user }, message: 'User found' }))
+    } catch (error) {
+        res.status(500).json(defaultResponse({ errorMessage: 'Error getting user', errorStatus: true }))
     }
 }
 
@@ -29,20 +92,24 @@ usersController.createUser = async (req, res) => {
     const { email, fullName, password, roleId } = req.body
 
     try {
+        const hashedPassword = await argon2.hash(password)
+
         const newUser = await User.create({
             email,
             fullName,
-            password,
+            password: hashedPassword,
             roleId
         })
 
-        res.json(newUser)
-    }catch (error) {
+        res.json(defaultResponse({ data: { user: newUser }, message: 'User created' }))
+    } catch (error) {
+        console.log(error)
         if (error.name === 'SequelizeUniqueConstraintError') {
-            return res.status(400).json({ message: "Email already exists" })
+            return res.status(400).json(defaultResponse({ errorMessage: 'Email already exists', errorStatus: true }))
         }
 
-        res.status(500).json({ message: "Error creating user" })
+        res.status(500).json(defaultResponse({ errorMessage: 'Error creating user', errorStatus: true })
+        )
     }
 }
 
@@ -55,7 +122,7 @@ usersController.updateUser = async (req, res) => {
         const user = await User.findByPk(id)
 
         if (!user) {
-            return res.status(404).json({ message: "User not found" })
+            return res.status(404).json(defaultResponse({ errorMessage: 'User not found', errorStatus: true }))
         }
 
         user.email = email
@@ -65,13 +132,13 @@ usersController.updateUser = async (req, res) => {
 
         await user.save()
 
-        res.json(user)
-    }catch (error) {
+        res.json(defaultResponse({ data: { user: user }, message: 'User updated' }))
+    } catch (error) {
         if (error.name === 'SequelizeUniqueConstraintError') {
-            return res.status(400).json({ message: "Email already exists" })
+            return res.status(400).json(defaultResponse({ errorMessage: 'Email already exists', errorStatus: true }))
         }
 
-        res.status(500).json({ message: "Error updating user" })
+        res.status(500).json(defaultResponse({ errorMessage: 'Error updating user', errorStatus: true }))
     }
 
 }
@@ -83,13 +150,13 @@ usersController.deleteUser = async (req, res) => {
         const user = await User.findByPk(id)
 
         if (!user) {
-            return res.status(404).json({ message: "User not found" })
+            return res.status(404).json(defaultResponse({ errorMessage: 'User not found', errorStatus: true }))
         }
 
         await user.destroy()
 
-        res.json({ message: "User deleted" })
-    }catch (error) {
-        res.status(500).json({ message: "Error deleting user" })
+        res.json(defaultResponse({ message: 'User deleted' }))
+    } catch (error) {
+        res.status(500).json(defaultResponse({ errorMessage: 'Error deleting user', errorStatus: true }))
     }
 }
