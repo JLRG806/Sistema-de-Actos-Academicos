276962d903cfe5dfec5e48871e8e9492dda99fdd
José Luis Rodríguez <jlrg.dev@gmail.com>
2024-12-17 17:08:12 +0100

    refactor: Simplify Sequelize initialization to use DATABASE_URL for database connection

--- back-end/db/db.js
+++ back-end/db/db.js
@@ -1,18 +1,10 @@
 import { Sequelize } from 'sequelize'
 import config from '../config/config.js'
 
-export const sequelize = new Sequelize(
-    config.db.postgres.options.database,
-    config.db.postgres.options.user,
-    config.db.postgres.options.password,
-    {
-        host: config.db.postgres.options.host,
-        dialect: config.db.postgres.options.dialect
-    }
-)
+export const sequelize = new Sequelize(config.db.postgres.DATABASE_URL)
 
 export async function connect() {
-    
+
     try {
         await sequelize.authenticate()
         console.log('Connection has been established successfully.')
