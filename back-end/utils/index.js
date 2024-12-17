a1c166a7a432a66a3a5e5132cee7711beb06f1ec
JLRG806 <jose.rodriguez2@gmail.com>
2024-12-17 01:09:55 +0100

    feat: Add utility functions for administrator check and standardized response object

---
+++ back-end/utils/index.js
@@ -0,0 +1,44 @@
+import config from '../config/config.js'
+
+// Javadoc style comments
+/**
+ * Check if the user is an administrator
+ * @param {string} email - The email of the user
+ * @param {string} password - The password of the user
+ * @returns {boolean} - True if the user is an administrator, false otherwise
+ */
+export const isAdministrator = async (email, password) => {
+    const adminEmail = config.admin.email
+    const adminPassword = config.admin.password
+    if (email === adminEmail && password === adminPassword) {
+        return true
+    }
+    return false
+}
+
+// Javadoc style comments
+/**
+ * Generates a standardized response object.
+ *
+ * @param {Object} params - The parameters for the response.
+ * @param {*} [params.data=null] - The data payload for the response.
+ * @param {string} [params.message=''] - A general success or informational message.
+ * @param {string} [params.errorMessage=''] - A message describing an error.
+ * @param {boolean} [params.errorStatus=false] - Indicates if an error occurred.
+ * @returns {Object} The standardized response object.
+ * @returns {Object} error - The error details.
+ * @returns {string} error.message - The error message.
+ * @returns {boolean} error.status - The error status.
+ * @returns {*} data - The data payload.
+ * @returns {string} message - The general message.
+ */
+export const defaultResponse = ({ data = null, message = '', errorMessage = '', errorStatus = false }) => {
+    return {
+        error: {
+            message: errorMessage,
+            status: errorStatus
+        },
+        data: data,
+        message: message
+    }
+}
