d03b322522cd7431e1e1eddb3fed079f6ffc0749
JLRG806 <jose.rodriguez2@gmail.com>
2024-12-17 01:10:09 +0100

    feat: Add login and registration routes to users controller

--- back-end/routes/users.routes.js
+++ back-end/routes/users.routes.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
 import { usersController } from "../controllers/users.controllers.js";
+import { cookieJWTauthAdmin, cookieJWTauthUser } from "../middlewares/cookieJWTauth.js";
 const router = Router();
+
+router.get("/login", usersController.login)
+router.post("/register", usersController.createUser)
 
 router.get("/users", usersController.getUsers)
 router.get("/users/:id", usersController.getUser)
