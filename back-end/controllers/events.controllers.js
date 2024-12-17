d0312ee64e63c1d8ca75140b75f1560795ea2689
JLRG806 <jose.rodriguez2@gmail.com>
2024-12-17 01:11:27 +0100

    feat: Refactor events controller to use standardized response format and handle user associations

--- back-end/controllers/events.controllers.js
+++ back-end/controllers/events.controllers.js
@@ -1,12 +1,19 @@
 import { Event } from "../models/sequelize/Event.js";
+import { defaultResponse } from "../utils/index.js";
 export const eventsController = {}
 
 eventsController.getEvents = async (req, res) => {
     try {
         const events = await Event.findAll()
-        res.json(events)
+
+        for (const event of events) {
+            const users = await event.getUsers()
+            event.dataValues.users = users
+        }
+        res.json(defaultResponse({ data: { events: events }, message: 'Events found' }))
     } catch (error) {
-        res.status(500).json({ message: "Error getting events" })
+        console.log(error)
+        res.status(500).json(defaultResponse({ errorMessage: 'Error getting events', errorStatus: true }))
     }
 }
 
@@ -16,15 +23,15 @@ eventsController.getEvent = async (req, res) => {
         const event = await Event.findByPk(id)
 
         if (!event) {
-            return res.status(404).json({ message: "Event not found" })
+            return res.status(404).json(defaultResponse({ errorMessage: 'Event not found', errorStatus: true }))
         }
 
         const userEvents = await event.getUsers()
 
-        res.json({ event: event, users: userEvents })
+        res.json(defaultResponse({ data: { event: event, users: userEvents }, message: 'Event found' }))
     } catch (error) {
-        console.log(error) 
-        res.status(500).json({ message: "Error getting event" })
+        console.log(error)
+        res.status(500).json(defaultResponse({ errorMessage: 'Error getting event', errorStatus: true }))
     }
 }
 
@@ -46,23 +53,25 @@ eventsController.createEvent = async (req, res) => {
 
         const userEvents = await newEvent.getUsers()
 
-        res.json({ event: newEvent, users: userEvents })
+        newEvent.dataValues.users = userEvents
+
+        res.json(defaultResponse({ data: { event: newEvent }, message: 'Event created' }))
     } catch (error) {
         console.log(error)
-        res.status(500).json({ message: "Error creating event" })
+        res.status(500).json(defaultResponse({ errorMessage: 'Error creating event', errorStatus: true }))
     }
 }
 
 eventsController.updateEvent = async (req, res) => {
 
     const { id } = req.params
-    const { title, description, start_date, end_date, location, status, userId } = req.body
+    const { title, description, start_date, end_date, location, status, userIdList } = req.body
 
     try {
         const event = await Event.findByPk(id)
 
         if (!event) {
-            return res.status(404).json({ message: "Event not found" })
+            return res.status(404).json(defaultResponse({ errorMessage: 'Event not found', errorStatus: true }))
         }
 
         event.title = title
@@ -71,13 +80,17 @@ eventsController.updateEvent = async (req, res) => {
         event.end_date = end_date
         event.location = location
         event.status = status
-        event.userId = userId
         await event.save()
 
-        res.json(event)
+        let userEvents = await newEvent.addUsers(userIdList)
+        userEvents = await newEvent.getUsers()
+
+        newEvent.dataValues.users = userEvents
+
+        res.json(defaultResponse({ data: { event: newEvent }, message: 'Event updated' }))
     } catch (error) {
         console.log(error)
-        res.status(500).json({ message: "Error updating event" })
+        res.status(500).json(defaultResponse({ errorMessage: 'Error updating event', errorStatus: true }))
     }
 }
 
@@ -88,12 +101,12 @@ eventsController.deleteEvent = async (req, res) => {
         const event = await Event.findByPk(id)
 
         if (!event) {
-            return res.status(404).json({ message: "Event not found" })
+            return res.status(404).json(defaultResponse({ errorMessage: 'Event not found', errorStatus: true }))
         }
 
         await event.destroy()
-        res.json({ message: "Event deleted" })
+        res.json(defaultResponse({ message: 'Event deleted' }))
     } catch (error) {
-        res.status(500).json({ message: "Error deleting event" })
+        res.status(500).json(defaultResponse({ errorMessage: 'Error deleting event', errorStatus: true }))
     }
 }
